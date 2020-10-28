e881f231a2c9a2289a3c31dfb408cb5a2ed19f84
Ali Khaleqi Yekta <me@yekta.dev>
2020-10-28 14:15:21 +0330

    Add RandomInt function

--- RanGo.go
+++ RanGo.go
@@ -8,17 +8,29 @@ import (
 	"time"
 )
 
-func RnInt(startIncluded int, endNotIncluded int) int {
+func RnInt(startIncluded int, endNotIncluded int) (randomInt int) {
 	rnInit()
-	return mathRand.Intn(endNotIncluded-startIncluded) + startIncluded
+	randomInt = startIncluded + mathRand.Intn(endNotIncluded-startIncluded)
+
+	return randomInt
 }
-func rnInit() {
+func RandomInt(startIncluded int, endNotIncluded int) (randomInt int, isSeedTimeDependent bool) {
+	isSeedTimeDependent = rnInit()
+	randomInt = startIncluded + mathRand.Intn(endNotIncluded-startIncluded)
+
+	return randomInt, isSeedTimeDependent
+}
+func rnInit() (isSeedTimeDependent bool) {
 	var b [8]byte
 	_, err := cryptoRand.Read(b[:])
 	if err != nil {
 		log.Println("Unable to generate seed with cryptographically secure random number generator. Time-dependent seed has been used instead.")
 		mathRand.Seed(time.Now().UnixNano())
+
+		return true
 	} else {
 		mathRand.Seed(int64(binary.LittleEndian.Uint64(b[:])))
+
+		return false
 	}
 }
--- RanGo_test.go
+++ RanGo_test.go
@@ -17,3 +17,21 @@ func TestRnInt(t *testing.T) {
 		}
 	}
 }
+func TestRandomInt(t *testing.T) {
+	int1, _ := RandomInt(0, 88)
+	int2, _ := RandomInt(0, 88)
+
+	if int1 == int2 {
+		t.Errorf("RandomInt function generates the same output each time!")
+	}
+	for i := 0; i < 10; i++ {
+		int3, _ := RandomInt(0, 1)
+
+		if int3 > 0 {
+			t.Errorf("RandomInt function generates an output greater than the greatest int allowed!")
+		}
+		if int3 < 0 {
+			t.Errorf("RandomInt function generates an output smaller than the smallest int allowed!")
+		}
+	}
+}
