de996e17c9d9664d70b9abcbf241e745a469ffa2
Ali Khaleqi Yekta <me@yekta.dev>
2020-10-28 14:53:12 +0330

    Add string generator

--- RanGo.go
+++ RanGo.go
@@ -8,6 +8,12 @@ import (
 	"time"
 )
 
+//Characters of random string generator
+var charsLowercase = "abcdefghijklmnopqrstuvwxyz"
+var charsUppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var charsNumber = "0123456789"
+var charsSpecial = "[](){}^;:#?.,&|!~`_%$@\"'=+-*/\\ <>"
+
 func RnInt(startIncluded int, endNotIncluded int) (randomInt int) {
 	rnInit()
 	randomInt = startIncluded + mathRand.Intn(endNotIncluded-startIncluded)
@@ -20,6 +26,95 @@ func RandomInt(startIncluded int, endNotIncluded int) (randomInt int, isSeedTime
 
 	return randomInt, isSeedTimeDependent
 }
+func RnString(length int, containsLowercase bool, containsUppercase bool, containsNumber bool, containsSpecial bool) (randomString string) {
+	if !containsLowercase && !containsUppercase && !containsNumber && !containsSpecial {
+		return ""
+	}
+	if length < 1 {
+		return ""
+	}
+
+	var chars []rune
+
+	if containsLowercase {
+		chars = append(chars, []rune(charsLowercase)...)
+	}
+	if containsUppercase {
+		chars = append(chars, []rune(charsUppercase)...)
+	}
+	if containsNumber {
+		chars = append(chars, []rune(charsNumber)...)
+	}
+	if containsSpecial {
+		chars = append(chars, []rune(charsSpecial)...)
+	}
+
+	rnInit()
+
+	b := make([]rune, length)
+	for i := range b {
+		b[i] = chars[mathRand.Intn(len(chars))]
+	}
+	return string(b)
+}
+func RandomString(length int, containsLowercase bool, containsUppercase bool, containsNumber bool, containsSpecial bool) (randomString string, isSeedTimeDependent bool) {
+	if !containsLowercase && !containsUppercase && !containsNumber && !containsSpecial {
+		return "", false
+	}
+	if length < 1 {
+		return "", false
+	}
+
+	var chars []rune
+
+	if containsLowercase {
+		chars = append(chars, []rune(charsLowercase)...)
+	}
+	if containsUppercase {
+		chars = append(chars, []rune(charsUppercase)...)
+	}
+	if containsNumber {
+		chars = append(chars, []rune(charsNumber)...)
+	}
+	if containsSpecial {
+		chars = append(chars, []rune(charsSpecial)...)
+	}
+
+	isSeedTimeDependent = rnInit()
+
+	b := make([]rune, length)
+	for i := range b {
+		b[i] = chars[mathRand.Intn(len(chars))]
+	}
+	return string(b), isSeedTimeDependent
+}
+func RnStringFrom(length int, chars string) (randomString string) {
+	if length < 1 {
+		return ""
+	}
+
+	rnInit()
+
+	b := make([]rune, length)
+	for i := range b {
+		b[i] = []rune(chars)[mathRand.Intn(len([]rune(chars)))]
+	}
+	return string(b)
+}
+func RandomStringFrom(length int, chars string) (randomString string, isSeedTimeDependent bool) {
+	if length < 1 {
+		return "", false
+	}
+
+	isSeedTimeDependent = rnInit()
+
+	b := make([]rune, length)
+	for i := range b {
+		b[i] = []rune(chars)[mathRand.Intn(len([]rune(chars)))]
+	}
+	return string(b), isSeedTimeDependent
+}
+
 func rnInit() (isSeedTimeDependent bool) {
 	var b [8]byte
 	_, err := cryptoRand.Read(b[:])
--- RanGo_test.go
+++ RanGo_test.go
@@ -35,3 +35,44 @@ func TestRandomInt(t *testing.T) {
 		}
 	}
 }
+func TestRnString(t *testing.T) {
+	if RnString(8, true, true, true, true) == RnString(8, true, true, true, true) {
+		t.Errorf("RnString function generates the same output each time!")
+	}
+
+	if len(RnString(88, true, true, true, true)) != 88 {
+		t.Errorf("RnString function generates an output with an unexpected length!")
+	}
+}
+func TestRandomString(t *testing.T) {
+	string1, _ := RandomString(8, true, true, true, true)
+	string2, _ := RandomString(8, true, true, true, true)
+	if string1 == string2 {
+		t.Errorf("RandomString function generates the same output each time!")
+	}
+
+	if len(string1) != 8 {
+		t.Errorf("RandomString function generates an output with an unexpected length!")
+	}
+}
+func TestRnStringFrom(t *testing.T) {
+	if RnStringFrom(8, "0123456789") == RnStringFrom(8, "0123456789") {
+		t.Errorf("RnStringFrom function generates the same output each time!")
+	}
+
+	if len(RnStringFrom(88, "01")) != 88 {
+		t.Errorf("RnStringFrom function generates an output with an unexpected length!")
+	}
+}
+func TestRandomStringFrom(t *testing.T) {
+	string1, _ := RandomStringFrom(8, "0123456789")
+	string2, _ := RandomStringFrom(8, "0123456789")
+
+	if string1 == string2 {
+		t.Errorf("RandomStringFrom function generates the same output each time!")
+	}
+
+	if len(string1) != 8 {
+		t.Errorf("RandomStringFrom function generates an output with an unexpected length!")
+	}
+}
